d6894a07aa115f5b763891b8cfa76bf405cf0b5d
io <micorreo.erick@gmail.com>
2016-09-14 15:53:15 -0400

    primera subida

---
+++ Source/BKB/Assets/Scripts/Player.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	Rigidbody _rb;
+
+	float _velocidad = 9f;
+	float _fuerzaDeSalto = 99f;
+	bool _tocandoPiso;
+
+	void Start () {
+		_rb = GetComponent<Rigidbody> ();
+		_tocandoPiso = true;
+	}
+	
+	void Update () {
+		float Horizontal = Input.GetAxis ("Horizontal");
+		float velocidad = _velocidad * Horizontal * Time.deltaTime;
+		bool Salto = Input.GetButton ("Jump");
+
+		transform.Translate (velocidad, 0, 0);
+
+		if (Salto && _tocandoPiso) {
+			_rb.AddForce (new Vector3 (0, _fuerzaDeSalto, 0));
+			_tocandoPiso = false;
+		}
+		Debug.Log (_tocandoPiso);
+	}
+
+	void OnCollisionEnter(Collision coll){
+		if (coll.collider.tag == "Platform") {
+			_tocandoPiso = true;
+		}
+	}
+}
